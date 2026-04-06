befcbbdb50e765e2bed350f0deebe00c38778831
josh <auroraspectrainfo@gmail.com>
2026-04-07 00:35:37 +0100

    logo added

--- sw.js
+++ sw.js
@@ -9,3 +9,12 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
   // You can cache requests here for offline use
 });
+
+const cacheAssets = [
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './assets/192x192.png',
+  './assets/512x512.png'
+];
